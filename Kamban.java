9678d755147b60a342b54e9d80eacb4b54c8435e
DanielrCardoso <daniel.dcuv@gmail.com>
2024-10-19 15:16:27 -0300

    configuracao incial spring

---
+++ Kamban.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kamban {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KambanApplication.class, args);
+    }
+}
