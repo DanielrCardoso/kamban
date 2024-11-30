7ad87a81dfa385c2b39f74a96e96aa182779ed67
douglas-lab <dn61967@gmail.com>
2024-11-30 12:42:43 -0300

    add new settings

---
+++ kambam novo/kamban/src/main/java/br/com/springboot/investimento/Application.java
@@ -0,0 +1,16 @@
+package br.com.springboot.investimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
